734a4b6e3a4f7ba25d5e498a2325a731354c904e
pradyumnap <pradyumnaraje.patil@gmail.com>
2021-01-26 11:23:33 +0530

    AVATAR DIALOG

--- src/components/reusable/AvatarComponent.js
+++ src/components/reusable/AvatarComponent.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function AvatarComponent({bg,image}) {
     return (
-        <div className="p-0 text-center position-relative w-100">
+        <div className="p-0 text-center position-relative w-100 avatar-demo">
             <img src={bg} className="w-100"/>
             <img src={image} className="profile-image  position-absolute bg-white"/>
         </div>
--- src/components/reusable/DialogComponent.js
+++ src/components/reusable/DialogComponent.js
@@ -39,7 +39,7 @@ function DialogComponent({open,handleClose,}) {
             <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
               <CloseIcon />
             </IconButton>
-            <Typography variant="h6" style={{flex: 1}}>
+            <Typography variant="div" style={{flex: 1}}>
               Choose An Avatar
             </Typography>
             <Button color="light" variant="contained" className="text-right text-dark" autoFocus color="inherit" onClick={handleClose}>
@@ -47,27 +47,48 @@ function DialogComponent({open,handleClose,}) {
             </Button>
           </Toolbar>
         </AppBar>
-        <div className="container-fluid mt-3 ">
+        <div className="container-fluid mt-3 mb-5">
+          <form>
           <div className="row">
-            <div className="col-lg-4 col-md-6 col-sm-6 col-12 mt-5 p-2">
-                <AvatarComponent bg={AvatarBG1} image={AvatarImage1} />
+            <div className="col-lg-4 col-md-6 col-sm-6 col-12 mt-5 p-2" style={{cursor: "pointer"}}>
+                <input name="avatar" type="radio" id="myCheckbox1" value={true} className="avatar-checkbox" />
+                <label for="myCheckbox1">
+                  <AvatarComponent bg={AvatarBG1} image={AvatarImage1} />
+                </label>
             </div>
             <div className="col-lg-4 col-md-6 col-sm-6 col-12 mt-5 p-2">
+            <input name="avatar"  type="radio" id="myCheckbox2" value={true} className="avatar-checkbox" />
+                <label for="myCheckbox2">
                 <AvatarComponent bg={AvatarBG2} image={AvatarImage2} />
+                </label>
             </div>
             <div className="col-lg-4 col-md-6 col-sm-6 col-12 mt-5 p-2">
-                <AvatarComponent bg={AvatarBG3} image={AvatarImage3} />
+            <input name="avatar"  type="radio" id="myCheckbox3" value={true} className="avatar-checkbox" />
+                <label for="myCheckbox3">
+                <AvatarComponent bg={AvatarBG3} image={AvatarImage3} />                </label>
+
             </div>
             <div className="col-lg-4 col-md-6 col-sm-6 col-12 mt-5 p-2">
-                <AvatarComponent bg={AvatarBG4} image={AvatarImage4} />
+            <input name="avatar"  type="radio" id="myCheckbox4" value={true} className="avatar-checkbox" />
+                <label for="myCheckbox4">
+                <AvatarComponent bg={AvatarBG4} image={AvatarImage4} />                </label>
+
             </div>
             <div className="col-lg-4 col-md-6 col-sm-6 col-12 mt-5 p-2 ">
-                <AvatarComponent bg={AvatarBG5} image={AvatarImage5} />
+            <input name="avatar"  type="radio" id="myCheckbox5" value={true} className="avatar-checkbox" />
+                <label for="myCheckbox5">
+                <AvatarComponent bg={AvatarBG5} image={AvatarImage5} />                </label>
+
             </div>
             <div className="col-lg-4 col-md-6 col-sm-6 col-12 mt-5 p-2">
-                <AvatarComponent bg={AvatarBG6} image={AvatarImage6} />
+                <input name="avatar"  type="radio" id="myCheckbox6" value={true} className="avatar-checkbox" />
+                    <label for="myCheckbox6">
+                <AvatarComponent bg={AvatarBG6} image={AvatarImage6} />                </label>
+
             </div>
+            
           </div>
+          </form>
         </div>
       </Dialog>
     )
