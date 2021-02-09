5bd14f371a44b834521298ee92c55834899084c7
pradyumnap <pradyumnaraje.patil@gmail.com>
2021-02-09 15:42:59 +0530

    CHECKING CHANGES

--- src/components/reusable/AvatarComponent.js
+++ src/components/reusable/AvatarComponent.js
@@ -4,7 +4,7 @@ function AvatarComponent({bg,image}) {
     return (
         <div className="p-0 text-center position-relative w-100 avatar-demo">
             <img src={bg} className="w-100"/>
-            <img src={image} className="profile-image  position-absolute bg-white"/>
+            <img src={image} className="profile-image position-absolute bg-white"/>
         </div>
     )
 }
