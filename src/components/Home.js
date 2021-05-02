92db26dab78b316c86a3259afcc94d3f1928f917
Abhibhaw <asthana3110@gmail.com>
2021-05-03 04:54:41 +0530

    Migrate npm to yarn

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,35 @@
-import React,{ useEffect,useState } from 'react'
-import { useSelector } from 'react-redux';
-import firebase from '../firebase/firebase';
-import '../App.css';
-import Course from '../images/Courses.svg';
-import { Button } from '@material-ui/core';
+import React from "react";
+import "../App.css";
+import { Button } from "@material-ui/core";
+import Course from "../images/Courses.svg";
 
 function Home() {
-    return (
-        <div>
-            <div className="container-fluid mt-5">
-                <div className="row">
-                    <div className="col-md-6 col-12 my-auto text-left">
-                        <h2 className="heading">Who are We?</h2>
-                        <p className="text-left course-content">
-                        We provide Mentorship and Guidance for FRM Level 1 exam conducted by GARP.
-                        </p>
-                        <Button className="mt-2 text-left rounded-pill mb-3 mb-md-0 theme-background" size="large" variant="contained" color="primary">Expore Courses</Button>
-                    </div>
-                    <div className="col-md-6 col-12">
-                        <img src={Course} className="img-fluid"/>
-                    </div>
-                </div>
-            </div>
-
+  return (
+    <div>
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <div className="col-md-6 col-12 my-auto text-left">
+            <h2 className="heading">Who are We?</h2>
+            <p className="text-left course-content">
+              We provide Mentorship and Guidance for FRM Level 1 exam conducted
+              by GARP.
+            </p>
+            <Button
+              className="mt-2 text-left rounded-pill mb-3 mb-md-0 theme-background"
+              size="large"
+              variant="contained"
+              color="primary"
+            >
+              Expore Courses
+            </Button>
+          </div>
+          <div className="col-md-6 col-12">
+            <img alt="front-img" src={Course} className="img-fluid" />
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
